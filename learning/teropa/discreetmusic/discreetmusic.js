20c54d40d5c1ec581dd11048bdf3f67ecf0533de
kdutia <kalyan.dutia@gmail.com>
2018-02-26 22:09:07 +0000

    teropa tutorials. discreet music not working

---
+++ learning/teropa/discreetmusic/discreetmusic.js
@@ -0,0 +1,11 @@
+//require(["node_modules/tone/build/Tone"],function(Tone){
+  /*var synth = new Tone.MonoSynth();
+  synth.toMaster();
+  synth.triggerAttackRelease('C4', '8n');*/
+
+  //create a synth and connect it to the master output (your speakers)
+  var synth = new Tone.Synth().toMaster()
+
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease('C4', '8n')
+//})
